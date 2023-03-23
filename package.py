8e08cc0e57b226ea31ee2f177051de23dc77eee4
Smurgledwerf <hughestopher86@gmail.com>
2023-03-23 16:33:48 -0700

    Create package.py

---
+++ package.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+name = 'photoshop_tools'
+
+version = '0.1.0'
+
+authors = ['smurgledwerf']
+
+description = 'Collection of photoshop scripts'
+
+requires = [
+    # a photoshop rez package that adds the executable to $PATH
+]
+
+def commands():
+    pass
