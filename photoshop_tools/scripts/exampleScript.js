3eeefb0202eb1b3b7d8becc4aca06c70e81ba23f
Smurgledwerf <hughestopher86@gmail.com>
2023-04-03 09:27:51 -0700

    Update exampleScript.js

--- photoshop_tools/scripts/exampleScript.js
+++ photoshop_tools/scripts/exampleScript.js
@@ -2,10 +2,17 @@
 Example script.
 */
 
+// If you have a script with common utility functions in a relative directory, you can do this to include it:
+#includepath "./"
+// this will give us the "utils" namespace
+#include "common_utils.js"
+// For some reason `#include "./common_utils.js"` does not work, and without the `#includepath` line
+// it will only look in photoshop's scripts directory
+
 /**
  * The script stub will call this main function when executed.
  */
 function main(){
     var doc = app.activeDocument;
-    alert(doc.name);
+    utils.annoy();
 }
