e31f2f9f3a3ece0fa95c8e88f0cd8f744640d3cf
Smurgledwerf <hughestopher86@gmail.com>
2023-04-03 09:28:35 -0700

    Update common_utils.js

--- photoshop_tools/scripts/common_utils.js
+++ photoshop_tools/scripts/common_utils.js
@@ -5,6 +5,7 @@ Common photoshop utility functions.
 // It isn't completely necessary to put these in a namespace, but it makes it easier to debug if it's clear
 // where a function is coming from.
 var utils = {
+    // I don't know why this is the correct syntax, but it's the only one that worked.
     annoy: function(){
         alert("Spam, Spam, Spam, Spam… Lovely Spam! Wonderful Spam!");
     }
