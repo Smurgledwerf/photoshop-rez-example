e985db2a32ced62b8815fc0df3ecbe47e58e4fde
Smurgledwerf <hughestopher86@gmail.com>
2023-04-03 09:22:50 -0700

    Create common_utils.js

---
+++ photoshop_tools/scripts/common_utils.js
@@ -0,0 +1,11 @@
+/*
+Common photoshop utility functions.
+*/
+
+// It isn't completely necessary to put these in a namespace, but it makes it easier to debug if it's clear
+// where a function is coming from.
+var utils = {
+    annoy: function(){
+        alert("Spam, Spam, Spam, Spam… Lovely Spam! Wonderful Spam!");
+    }
+}
